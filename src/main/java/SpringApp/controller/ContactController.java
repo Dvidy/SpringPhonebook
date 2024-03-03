1679f988b27c2c5c28cdf0cb91516241bf93b701
Dvidy <davidmarus18@gmail.com>
2024-03-03 18:56:20 +0100

    Změna z /api/contacts na /contacts

--- src/main/java/SpringApp/controller/ContactController.java
+++ src/main/java/SpringApp/controller/ContactController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @AllArgsConstructor
 @RestController
-@RequestMapping("/api/contacts")
+@RequestMapping("/contacts")
 public class ContactController {
 
     private ContactService contactService;
